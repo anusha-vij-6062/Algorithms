f665247637cb8afe5e5a3b2fa7d64c24e18249ed
Anusha-Vijay <anusha.vijay2@gmail.com>
2017-10-18 18:35:08 -0700

    Update expressionEvaluator.cpp

--- expressionEvaluator.cpp
+++ expressionEvaluator.cpp
@@ -1,3 +1,11 @@
+// # You are building an educational website and want to create a simple calculator for students to use. For now, the calculator will only allow addition and subtraction of positive integers.
+
+// # Given an expression string using the "+" and "-" operators like "5+16-2", write a function to parse the string and evaluate the result.
+
+// # Sample input/output:
+// # "6+9-12" => 3
+// # "1+2-3+4-5+6-7" => -2
+
 #include <iostream>
 #include <queue>
 #include<sstream>
@@ -10,7 +18,7 @@ int evaluate(string expression){
     queue<char>e1;
     bool first=true;
     int ans=0;
-    int num1,num2;
+    int num;
     int startIndex=0;
     int currentIndex=0;
 
@@ -30,45 +38,31 @@ int evaluate(string expression){
     int x;
     istringstream (substring) >> x;
     q1.push(x);
-
-//   // case for + and -
-//   // get the expression 
+    ans=q1.front();
+    q1.pop();
 
     while(!q1.empty()){
         char sign=e1.front();
         e1.pop();
-        if(first){
-            num1=q1.front();
-            if(sign=='+')
-                ans+=num1;
-            else
-                ans-=num1;
-            q1.pop();
-        }
         switch(sign){
             case '+':
-                num2=q1.front();
-                ans+=num2;
+                num=q1.front();
+                ans+=num;
                 q1.pop();
                 break;
             case '-':
-                num2=q1.front();
-                ans-=num2;
+                num=q1.front();
+                ans-=num;
                 q1.pop();
                 break;
         }
    }
     return ans;
 }
-
-
-
 // To execute C++, please define "int main()"
 int main() {
-    string expression = "6+90";
+    string expression = "6+90-9+10+3";
     cout<<evaluate(expression);
-
-
     return 0;
 }
 
